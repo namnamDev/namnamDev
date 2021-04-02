e486c069c198d4ff8cc33bd84d3e2bf61600a488
남근형 <magicom98@gmail.com>
2021-04-02 20:42:55 +0900

    0402fir

--- Programers_2020 KAKAO BLIND RECRUITMENT_2.py
+++ Programers_2020 KAKAO BLIND RECRUITMENT_2.py
@@ -1,17 +1,17 @@
 def cycle(arr):
-    N=len(arr)
-    board = [[0]*N for _ in range(N)]
+    N = len(arr)
+    board = [[0] * N for _ in range(N)]
     for i in range(N):
         for h in range(N):
             one = arr[i][h]
-            board[h][N-1-i] = one
+            board[h][N - 1 - i] = one
     return board
-            
+
 
 def solution(key, lock):
-    # key = [[0,1,0],[0,1,0],[0,1,0]]
-    # lock = [[0,0,0,0],[0,0,0,1],[0,0,1,0],[1,0,0,0]]
-    N, M = len(key) , len(lock)
+    # key = [[1,1,1],[1,1,1],[1,1,1]]
+    # lock = [[0,0,0,],[0,0,0],[0,0,0]]
+    N, M = len(key), len(lock)
     # print('key')
     # for i in key:
     #     print(i)
@@ -20,35 +20,37 @@ def solution(key, lock):
     #     print(g)
     answer = False
     for f in range(4):
-        temp =[[0]*M for _ in range(M)]
-        for i in range(M):
-            for g in range(M):
-                temp[i][g] = lock[i][g]
         key = cycle(key)
         Q = []
         for i in range(N):
             for g in range(N):
                 if key[i][g]:
-                    Q += [[i,g]]
+                    Q += [[i, g]]
         # print(Q)
-        for ii in range(-M-N,M+N):
-            for gg in range(-M-N,M+N):
+        for ii in range(-M + 1, M):
+            for gg in range(-M + 1, M):
+                temp = [[0] * M for _ in range(M)]
+
+                for i in range(M):
+                    for g in range(M):
+                        temp[i][g] = lock[i][g]
+
                 for qq in Q:
-                    keyy,keyx = qq[0],qq[1]
-                    if 0<=ii+keyy<M and 0<=gg+keyx<M:
-                        temp[ii+keyy][gg+keyx]=1
+                    keyy, keyx = qq[0], qq[1]
+                    if 0 <= ii + keyy < M and 0 <= gg + keyx < M:
+                        if temp[ii + keyy][gg + keyx]:
+                            break
+                        else:
+                            temp[ii + keyy][gg + keyx] = 1
                 # print(ii,gg,temp)
                 cnt = 0
                 for i in temp:
                     if 0 not in i:
-                        cnt+=1
+                        cnt += 1
+
                 if cnt == M:
                     answer = True
                     break
-                else:
-                    for i in range(M):
-                        for g in range(M):
-                            temp[i][g] = lock[i][g]
             if answer:
                 break
         if answer:
