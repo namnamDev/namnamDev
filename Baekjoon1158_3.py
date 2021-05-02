255dfd086f0424c034700c2e01bba4320ed104f2
남근형 <magicom98@gmail.com>
2021-05-02 23:16:38 +0900

    '0502fir'

---
+++ Baekjoon1158_3.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+li = list(range(1, N + 1))
+Q = []
+g = K
+start = 0
+while len(Q) != N:
+    start += g - 1
+    lenLi = len(li)
+    if lenLi <= start:
+        start = start % lenLi
+    Q.append(li.pop(start))
+an = str(Q)[1: -1]
+print("<" + an + ">")
