3c3ea362e89696ff131becadad08274851d0d5b2
남근형 <magicom98@gmail.com>
2021-06-01 22:19:24 +0900

    0601fir

--- Baekjoon1158_3.py
+++ Baekjoon1158_3.py
@@ -1,13 +1,15 @@
-N, K = map(int, input().split())
-li = list(range(1, N + 1))
-Q = []
-g = K
-start = 0
-while len(Q) != N:
-    start += g - 1
-    lenLi = len(li)
-    if lenLi <= start:
-        start = start % lenLi
-    Q.append(li.pop(start))
-an = str(Q)[1: -1]
-print("<" + an + ">")
+# N, K = map(int, input().split())
+# li = list(range(1, N + 1))
+# Q = []
+# g = K
+# start = 0
+# while len(Q) != N:
+#     start += g - 1
+#     lenLi = len(li)
+#     if lenLi <= start:
+#         start = start % lenLi
+#     Q.append(li.pop(start))
+# an = str(Q)[1: -1]
+# print("<" + an + ">")
+
+print('thx!')
