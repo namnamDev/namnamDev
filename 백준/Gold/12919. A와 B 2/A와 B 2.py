dab278ca85f70e4f072b9148517708c0bdfba5d1
namnam <74337668+namnamDev@users.noreply.github.com>
2022-04-13 10:22:48 +0900

    [Gold V] Title: A와 B 2, Time: 140 ms, Memory: 113764 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+start = input()
+end = input()
+end_size = len(end)
+start_size = len(start)
+Q = deque([end])
+an = 0
+used = {}
+while Q:
+    now = Q.pop()
+    if now == start:
+        an = 1
+        break
+    if len(now) > start_size:
+        if now[0] == "B":
+            b_now = now[:0:-1]
+            Q.append(b_now)
+
+        if now[-1] == "A":
+            a_now = now[:-1]
+            Q.append(a_now)
+print(an)
