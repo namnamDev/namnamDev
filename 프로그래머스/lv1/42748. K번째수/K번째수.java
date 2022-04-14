289755ec24a074aeed16a3b28fe9c74845528c0b
namnam <74337668+namnamDev@users.noreply.github.com>
2022-04-14 20:44:15 +0900

    [level 1] Title: K번째수, Time: 1.59 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length;i++){
+            int[] oneLine = commands[i];
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            System.out.printf("%d, %d",oneLine[0]-1,oneLine[1]-1);
+            for (int j = oneLine[0]-1; j < oneLine[1]; j++){
+                temp.add(array[j]);
+            }
+            Collections.sort(temp);
+            // temp.sort();
+            // System.out.println(temp[oneLine[2]-1]);
+            answer[i]= temp.get(oneLine[2]-1);
+        }
+        return answer;
+    }
+}
